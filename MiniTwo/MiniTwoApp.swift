9421a52d3bfeeb996128c72fcffa65833eb1618e
Yerik Koslowski <yerik.koslowski@gmail.com>
2023-08-25 11:43:56 -0300

    Initial Commit

---
+++ MiniTwo/MiniTwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniTwoApp.swift
+//  MiniTwo
+//
+//  Created by Yerik Koslowski on 25/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MiniTwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
