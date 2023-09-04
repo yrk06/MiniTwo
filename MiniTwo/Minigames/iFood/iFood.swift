47c0313b3b858add534d8e1fad5d73ae948c99c2
Gustavo Isoo Binder <gustan.binder@gmail.com>
2023-09-04 09:25:34 -0300

    add: iFood order screen.

--- MiniTwo/Minigames/iFood/iFood.swift
+++ MiniTwo/Minigames/iFood/iFood.swift
@@ -7,20 +7,194 @@
 
 import SwiftUI
 
-struct iFood: View {
+struct CardH : View {
+    
+    var verbatim : String
+    
+    init(_ verbatim: String) {
+        self.verbatim = verbatim
+    }
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            Text(verbatim)
+                .font(.caption2)
+                .padding()
+                .frame(width: 100)
+        }
+        .frame(width: 100, height: 100)
+        .background {
+            HStack {
+                Color.gray
+                    .opacity(0.2)
+            }
+        }
+        .cornerRadius(8)
+        .foregroundColor(.black)
+    }
+}
+
+struct CardV : View {
+    
+    let food : FoodCard
+    
+    init(food: FoodCard) {
+        self.food = food
+    }
+    
+    var body: some View {
+        VStack (alignment: .leading) {
+            Text(food.nome)
+                .font(.footnote)
+                .bold()
+                .padding(.bottom, 2)
+            Text(food.verbatim)
+                .font(.caption2)
+                .padding(.bottom, 4)
+                .frame(alignment: .leading)
+            Text("R$\(String(format: "%.2f", food.price))")
+                .font(.caption2)
+        }
+        .foregroundColor(.black)
+    }
+}
+
+struct FoodCard {
+    var nome : String
+    var nome_restaurante : String
+    var verbatim : String
+    var price : Float
+}
+
+var food_cards = [
+    FoodCard(nome: "Pepinada", nome_restaurante: "Casa do Pepino", verbatim: "Delicioso prato tipicamente brasileiro com um toque refrescante", price: 15.99),
+    FoodCard(nome: "Pizza de Pepino", nome_restaurante: "Pepino's Pizza", verbatim: "Para os loucos por pepino", price: 25.99),
+    FoodCard(nome: "Sanduíche de pepino", nome_restaurante: "PepinoWay", verbatim: "Do siri cascudo", price: 15.99),
+    FoodCard(nome: "Hamburger de pepino", nome_restaurante: "Pepino Donald's", verbatim: "Também do Siri Cascudo", price: 15.99)
+]
+
+struct iFood : View {
     
     @EnvironmentObject var objMan : ObjectiveManager
     
+    @State var last_ordered : [FoodCard] = [food_cards[0], food_cards[1], food_cards[2]]
+    
     var body: some View {
         ZStack {
-            
-            Button {
-                objMan.complete_mission(type: .comer)
-            } label: {
-                Text("Pedir comida")
+            ScrollView {
+                VStack (alignment: .leading) {
+                    VStack (alignment: .leading) {
+                        HStack {
+                            Text("30 dias grátis de cupons")
+                                .font(.caption2)
+                                .foregroundColor(.white)
+                                .padding()
+                                .background {
+                                    Color.init(uiColor: .orange)
+                                        .opacity(0.5)
+                                }
+                                .cornerRadius(31)
+                            Spacer()
+                        }
+                        Spacer()
+                        Text("Você ganhou até $50 em cupons grátis")
+                            .foregroundColor(.white)
+                            .font(.body)
+                            .bold()
+                            .frame(width: 200)
+                    }
+                    .padding()
+                    .background {
+                        Color("OrangeFood")
+                    }
+                    .cornerRadius(12)
+                    .padding()
+                    .frame(height: 225)
+                    
+                    Text("Tipos de cozinha")
+                        .font(.body)
+                        .bold()
+                        .padding()
+                    
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack {
+                            NavigationLink {
+                                iFoodOrderScreen(food: food_cards[1], last_ordered: $last_ordered)
+                            } label: {
+                                CardH("Italiana")
+                            }
+                            NavigationLink {
+                                iFoodOrderScreen(food: food_cards[0], last_ordered: $last_ordered)
+                            } label: {
+                                CardH("Brasileira")
+                            }
+                            NavigationLink {
+                                iFoodOrderScreen(food: food_cards[2], last_ordered: $last_ordered)
+                            } label: {
+                                CardH("Saudável")
+                            }
+                            NavigationLink {
+                                iFoodOrderScreen(food: food_cards[3], last_ordered: $last_ordered)
+                            } label: {
+                                CardH("Americana")
+                            }
+                        }
+                        .padding(.horizontal)
+                    }
+                    
+                    Text("Suas últimas refeições")
+                        .font(.body)
+                        .bold()
+                        .padding()
+                    
+                    HStack {
+                        NavigationLink {
+                            iFoodOrderScreen(food: last_ordered[0], last_ordered: $last_ordered)
+                        } label: {
+                            VStack {
+                                CardV(food: last_ordered[0])
+                                Spacer()
+                            }
+                            .padding()
+                            .background {
+                                Color.gray
+                                    .opacity(0.2)
+                            }
+                            .cornerRadius(8)
+                        }
+                        VStack {
+                            NavigationLink {
+                                iFoodOrderScreen(food: last_ordered[1], last_ordered: $last_ordered)
+                            } label: {
+                                CardV(food: last_ordered[1])
+                                    .padding()
+                                    .background {
+                                        Color.gray
+                                            .opacity(0.2)
+                                    }
+                                    .cornerRadius(8)
+                            }
+                            NavigationLink {
+                                iFoodOrderScreen(food: last_ordered[2], last_ordered: $last_ordered)
+                            } label: {
+                                CardV(food: last_ordered[2])
+                                    .padding()
+                                    .background {
+                                        Color.gray
+                                            .opacity(0.2)
+                                    }
+                                    .cornerRadius(8)
+                            }
+                        }
+                    }
+                    .frame(height: 195)
+                    .padding(.horizontal)
+                    
+                }
             }
             
-            ForEach(Range(1...10), id: \.self) { i in
+            ForEach(Range(1...5), id: \.self) { i in
                 let off : CGFloat = CGFloat(i)/10
                 FPopUp(text: phrases["Você merece!"]!, time_to_appear: .now() + off)
             }
---
+++ MiniTwo/Minigames/iFood/iFoodOrderScreen.swift
@@ -0,0 +1,94 @@
+//
+//  iFoodOrderScreen.swift
+//  MiniTwo
+//
+//  Created by Gustavo Binder on 01/09/23.
+//
+
+import SwiftUI
+
+struct iFoodOrderScreen: View {
+    
+    @EnvironmentObject var objMan : ObjectiveManager
+    @Environment(\.dismiss) var dismiss
+    
+    var food : FoodCard!
+    var name : String = "Muda Pizza"
+    var restaurantName : String = "Giorno's Pizza"
+    var description : String = "This is a placeholder description.\nLook at how it can support Markdown!!"
+    
+    @Binding var last_ordered : [FoodCard]
+    
+    var body: some View {
+        VStack (alignment: .leading) {
+            Rectangle()
+                .foregroundColor(.blue)
+            HStack {
+                Text(food.nome_restaurante)
+                    .font(.title)
+                    .bold()
+                Spacer()
+                Image(systemName: "star.fill")
+                Text("5,0")
+                    .font(.callout)
+            }
+            .padding(.horizontal)
+            .padding(.top)
+            .padding(.bottom, 4)
+            
+            Text(food.verbatim)
+                .font(.footnote)
+                .padding()
+            
+            Text("R$ \(String(format: "%.2f", food.price))")
+                .font(.title3)
+                .bold()
+                .padding(.horizontal)
+            
+            VStack (alignment: .leading) {
+                HStack {
+                    Image(systemName: "basket")
+                    Text(food.nome_restaurante)
+                    Spacer()
+                    Image(systemName: "star.fill")
+                    Text("5,0")
+                }
+                Divider()
+                HStack {
+                    Text("30-40min •")
+                    Text("Grátis")
+                        .foregroundColor(.red)
+                }
+                .font(.caption2)
+            }
+            .font(.caption2)
+            .padding()
+            .background()
+            .cornerRadius(12)
+            .padding()
+            .shadow(radius: 5)
+//            .border(Color.black.opacity(0.2))
+            
+            Divider()
+                .padding(.horizontal)
+            
+            Button {
+                objMan.complete_mission(type: .comer)
+                last_ordered.insert(food, at: 0)
+                last_ordered.remove(at: 3)
+                dismiss()
+            } label: {
+                Text("Comprar \(food.nome)")
+                    .frame(maxWidth: .infinity)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background {
+                        Color("OrangeFood")
+                    }
+                    .cornerRadius(12)
+                    .padding()
+            }
+
+        }
+    }
+}
